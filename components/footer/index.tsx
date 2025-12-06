3839e2a1648e2ba2e53146594a9a5739ce9a8298
Monika Szucs <monika.szucs.work@gmail.com>
2025-12-05 23:19:41 -0700

    updating text and layout

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
         <div className={styles.column}>
           <div className={styles.logoSection}>
             Logo
-            <p className={styles.logoText}>Your company description or tagline here.</p>
+            <p className={styles.logoText}>Connect every community.</p>
           </div>
         </div>
 
@@ -18,10 +18,10 @@ export default function Footer() {
         <div className={styles.column}>
           <h4 className={styles.columnTitle}>Getting Around</h4>
           <ul className={styles.linkList}>
-            <li><a href="/getting-around" className={styles.footerLink}>Getting around</a></li>
-            <li><a href="/transit-fares" className={styles.footerLink}>Transit Fares</a></li>
-            <li><a href="/schedules" className={styles.footerLink}>Schedules</a></li>
-            <li><a href="/amenities" className={styles.footerLink}>Amenities</a></li>
+            <li><a href="/transit-fares" className={styles.footerLink}>Points</a></li>
+            <li><a href="/schedules" className={styles.footerLink}>Map</a></li>
+            <li><a href="/amenities" className={styles.footerLink}>Book</a></li>
+            <li><a href="/amenities" className={styles.footerLink}>More</a></li>
           </ul>
         </div>
 
@@ -39,11 +39,7 @@ export default function Footer() {
         <div className={styles.column}>
           <h4 className={styles.columnTitle}>Contact Us</h4>
           <div className={styles.contactInfo}>
-            <p className={styles.contactText}>Call us at:</p>
-            <a href="tel:+1-555-123-4567" className={styles.contactLink}>+1 (555) 123-4567</a>
-            
-            <p className={styles.contactText}>Address:</p>
-            <p className={styles.address}>123 Main Street<br />City, State 12345</p>
+            <p className={styles.contactText}>test@test.com</p>
             
             <div className={styles.socialIcons}>
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
