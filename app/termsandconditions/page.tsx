326262da594862ace7a5f792387c7d652269183a
thechinookline <thechinookline@gmail.com>
2025-12-13 12:02:02 -0700

    T1010 getting started banner (#9)
    * [t1010] - created component for getting started earning points and fixed stylization in globals
    
    * [t1010] - saving color stylizing and linking to pages that i have setup

---
+++ app/termsandconditions/page.tsx
@@ -0,0 +1,21 @@
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import Subfooter from "../../components/subfooter";
+import Subheader from "../../components/subheader";
+import styles from "./termsandconditions.module.css";
+
+export default function TermsAndConditions() {
+  return (
+    <div className={styles.page}>
+      <Subheader />
+      <Header currentPage="home" />
+      
+      <main className={styles.main}>
+        Terms and conditions
+      </main>
+      
+      <Footer />
+      <Subfooter />
+    </div>
+  );
+}
