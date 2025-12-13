d39705708132d34c034aad20d796b222ec3d75ae
Monika Szucs <thechinookline@gmail.com>
2025-12-13 12:28:47 -0700

    [t1018] updated privacy policy page text and style

--- app/points/page.tsx
+++ app/points/page.tsx
@@ -11,14 +11,6 @@ export default function Points() {
       <Subheader/>
       <Header currentPage="points"/>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className={styles.intro}>
           <h1>Points</h1>
           <p>
