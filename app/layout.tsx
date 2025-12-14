5ea1b69fdaee6c254d8bd7a08b94593626e27b3a
Jashanraj <jashanrajj@gmail.com>
2025-12-14 14:27:28 -0800

    npm build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="topStack">
           <Subheader />
-          <Header currentPage="home" />
+          <Header />
         </div>
         {children}
       </body>
