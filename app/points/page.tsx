6d17b7043eb221da7667ae3e538f5b3a10748e7e
Monika Szucs <monika.szucs.work@gmail.com>
2025-12-18 20:41:52 -0700

    Created component for contact information for phone, operating hours and email (#18)
    Added the operationhours component to the home page, points, map and book page

--- app/points/page.tsx
+++ app/points/page.tsx
@@ -5,6 +5,7 @@ import Header from "../../components/header";
 import Subfooter from "../../components/subfooter";
 import Footer from "../../components/footer";
 import MessageBanner from "../../components/message";
+import OperationHours from "../../components/operationhours";
 
 export default function Points() {
     return (
@@ -59,6 +60,7 @@ export default function Points() {
             Documentation
           </a>
         </div>
+        <OperationHours/>
       </main>
       <Footer/>
       <MessageBanner/>
