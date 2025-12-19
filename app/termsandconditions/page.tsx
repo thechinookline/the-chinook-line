45f1547bfc8161ee86ce32e26d76debc13e217ef
TakamiInagaki <takami.l.inagaki@gmail.com>
2025-12-18 20:16:01 -0800

    initial commit

--- app/termsandconditions/page.tsx
+++ app/termsandconditions/page.tsx
@@ -12,7 +12,21 @@ export default function TermsAndConditions() {
       <Header currentPage="home" />
       
       <main className={styles.main}>
-        Terms and conditions
+        <h1 className={styles.title}>Terms and conditions</h1>
+        <div className={styles.termsandcondition}>
+        <p>
+          The Chinook Line welcomes you to its Web site. Using this Web site indicates your consent and agreement to be bound by this User Agreement and any policies or practices contained herein.
+        </p>
+        <p>
+          All content and information (including, but not limited to, code, graphics, photos, guides, promotional materials, press releases, articles, plans, schedules, illustrations, audio clips, and video clips) displayed on this Web site is owned or licensed by The Chinook Line and its respective owners, all content on this site is protected by copyright and other intellectual property laws. Any names, titles, logos and designs displayed on the Web site that contain The Chinook Line, titles, logos, or brands are the exclusive property and trademarks of The Chinook Line or its members. You may not modify, publish, transmit, transfer, sell, reproduce, re-post or alter any such content. The Chinook Line hereby grants you a personal, non-exclusive, non-assignable and non-transferable license to use and display, for home, non-commercial and personal use only, one copy of any content that you may download from this Web site. Any other use or reproduction of this Web site, in whole or in part, will be considered to be a violation of The Chinook Line intellectual property rights.
+        </p>
+        <p>
+          Your use of this Web site constitutes your agreement to abide by these terms of use included in this online User Agreement. The Chinook Line reserves the right to modify, alter or otherwise update this User Agreement at any time.
+        </p>
+        <p>
+          If you disagree with any of the above terms of use or any part of the User Agreement, do not use this Web site.
+        </p>
+        </div>
       </main>
       
       <Footer />
