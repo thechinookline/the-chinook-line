040826882ee69c8599f1d534b4bf1b961c6f1622
Monika Szucs <monika.szucs.work@gmail.com>
2025-12-24 12:16:41 -0700

    [T1026] - adding testing code and removed unwanted code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,26 +17,7 @@ export default function Login() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          Testing
         </div>
         <div className={styles.ctas}>
           <a
