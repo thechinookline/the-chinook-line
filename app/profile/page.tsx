cfed472603d4d34d3f1595f816a9c6317d5170c5
Monika Szucs <monika.szucs.work@gmail.com>
2025-12-25 13:48:27 -0700

    [T1029] - removed code not needed to fix pipeline issue

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,13 @@
 import ProfileInfo from "../../components/profileinfo";
-import Header from "../../components/header";
 import Footer from "../../components/footer";
 import Subfooter from "../../components/subfooter";
-import Subheader from "../../components/subheader";
 import styles from "./profile.module.css";
 import MessageBanner from "../../components/message";
 
-export default function Profile(){
+export default function Profile() {
     return(
 
     <div className={styles.page}>
-      <Subheader />
-      <Header/>
        <main>
         <ProfileInfo />
        </main>
